24d73c9cda1f7837d76aa3bfae65141a95c9ffd5
Yerold <112283864+yeroldsan@users.noreply.github.com>
2024-03-23 16:45:42 +0200

    feat: Implement sign up/in with supabase

--- app/src/main/java/fi/project/petcare/viewmodel/AuthViewModel.kt
+++ app/src/main/java/fi/project/petcare/viewmodel/AuthViewModel.kt
@@ -1,12 +1,93 @@
 package fi.project.petcare.viewmodel
 
+import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import fi.project.petcare.BuildConfig
+import io.github.jan.supabase.createSupabaseClient
+import io.github.jan.supabase.gotrue.Auth
+import io.github.jan.supabase.gotrue.ExternalAuthAction
+import io.github.jan.supabase.gotrue.auth
+import io.github.jan.supabase.gotrue.providers.builtin.Email
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
 
 class AuthViewModel: ViewModel() {
+
+    private val apiUrl = BuildConfig.SUPABASE_URL
+    private val apiKey = BuildConfig.SUPABASE_KEY
+
+
+    val supabase = createSupabaseClient(apiUrl, apiKey) {
+        install(Auth) {
+            host = "fi.project.petcare"
+            scheme = "deeplink scheme"
+            defaultExternalAuthAction = ExternalAuthAction.CUSTOM_TABS //defaults to EXTERNAL_BROWSER
+        }
+    }
+
+    fun signUp(userEmail: String, userPassword: String) {
+        closeBottomSheet()
+        viewModelScope.launch {
+            try {
+                val user = supabase.auth.signUpWith(Email) {
+                    email = userEmail
+                    password = userPassword
+                }
+                Log.i("User registered returns: ", user.toString())
+            } catch (e: Exception) {
+                Log.e("User registration failed: ", e.toString())
+            }
+        }
+    }
+
+    fun signIn(userEmail: String, userPassword: String) {
+        closeBottomSheet()
+        viewModelScope.launch {
+            try {
+                val user = supabase.auth.signInWith(Email) {
+                    email = userEmail
+                    password = userPassword
+                }
+                Log.i("User signed in returns: ", user.toString())
+            } catch (e: Exception) {
+                Log.e("User sign in failed: ", e.toString())
+            }
+        }
+    }
+
+    fun resetPassword(userEmail: String, userPassword: String) {
+        closeBottomSheet()
+        viewModelScope.launch {
+            try {
+                val user = supabase.auth.resetPasswordForEmail(userEmail)
+                Log.i("User reset password returns: ", user.toString())
+
+                /* TODO: Implement reset password email
+                supabase.auth.modifyUser {
+                    password = userPassword
+                }
+                */
+
+
+            } catch (e: Exception) {
+                Log.e("User reset password failed: ", e.toString())
+            }
+        }
+    }
+
+    fun signOut() {
+        viewModelScope.launch {
+            try {
+                val user = supabase.auth.signOut()
+                Log.i("User signed out returns: ", user.toString())
+            } catch (e: Exception) {
+                Log.e("User sign out failed: ", e.toString())
+            }
+        }
+    }
+
     enum class AuthMode {
         LOGIN,
         REGISTER
@@ -31,19 +112,5 @@ class AuthViewModel: ViewModel() {
         _showBottomSheet.value = false
     }
 
-
-    fun signUp(email: String, password: String) {
-        closeBottomSheet()
-        viewModelScope.launch {
-            // TODO
-        }
-    }
-
-    fun signIn(email: String, password: String) {
-        closeBottomSheet()
-        viewModelScope.launch {
-            // TODO
-        }
-    }
 
 }
