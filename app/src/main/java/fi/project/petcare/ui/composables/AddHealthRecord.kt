fd8052390418cf9af9e57a68cfb4f481f3cb6ebc
t2haev00 <eveliinahampus@gmail.com>
2024-03-26 09:05:21 +0200

    chore: composables

---
+++ app/src/main/java/fi/project/petcare/ui/composables/AddHealthRecord.kt
@@ -0,0 +1,26 @@
+package fi.project.petcare.ui.composables
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+
+@Composable
+fun AddHealthRecord() {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        // Define UI elements for adding health records
+        Button(
+            onClick = { /* Handle button click */ }
+        ) {
+            Text("Add Health Record")
+        }
+    }
+}
---
+++ app/src/main/java/fi/project/petcare/ui/composables/ViewHealthRecords.kt
@@ -0,0 +1,21 @@
+package fi.project.petcare.ui.composables
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+
+@Composable
+fun ViewHealthRecords() {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        // Define UI elements for viewing health records
+        Text(text = "View Health Records")
+    }
+}
