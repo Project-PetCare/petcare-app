e2f4ae31d280549dc70bd1eb02faea772456d25c
Yerold <112283864+yeroldsan@users.noreply.github.com>
2024-03-27 19:52:13 +0200

    feat: Create bottom navigation bar

---
+++ app/src/main/java/fi/project/petcare/ui/composables/PetCareBottomBar.kt
@@ -0,0 +1,37 @@
+package fi.project.petcare.ui.composables
+
+import androidx.compose.material3.Icon
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.navigation.NavController
+import androidx.navigation.NavDestination.Companion.hierarchy
+import androidx.navigation.compose.currentBackStackEntryAsState
+import fi.project.petcare.ui.nav.petCareDestinations
+
+@Composable
+fun PetCareBottomBar(navController: NavController) {
+    NavigationBar {
+        val navBackStackEntry by navController.currentBackStackEntryAsState()
+        val currentDestination = navBackStackEntry?.destination
+
+        petCareDestinations.forEach { screen ->
+            NavigationBarItem(
+                icon = { screen.icon?.let { Icon(imageVector = it, contentDescription = screen.route) } },
+                label = { Text(text = screen.route) },
+                selected = currentDestination?.hierarchy?.any { it.route == screen.route } == true,
+                onClick = {
+                    navController.navigate(screen.route) {
+                        popUpTo(navController.graph.startDestinationId) {
+                            saveState = true
+                        }
+                        launchSingleTop = true
+                        restoreState = true
+                    }
+                }
+            )
+        }
+    }
+}
