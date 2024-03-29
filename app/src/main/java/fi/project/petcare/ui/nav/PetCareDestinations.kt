53aaeb1e065db1162b1c1bdf4df4649a5ab0a8f7
Yerold <112283864+yeroldsan@users.noreply.github.com>
2024-03-29 12:22:43 +0200

    feat: Add optional icon to screen when destination is selected

--- app/src/main/java/fi/project/petcare/ui/nav/PetCareDestinations.kt
+++ app/src/main/java/fi/project/petcare/ui/nav/PetCareDestinations.kt
@@ -1,6 +1,8 @@
 package fi.project.petcare.ui.nav
 
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Lock
 import androidx.compose.material.icons.outlined.Android
 import androidx.compose.material.icons.outlined.Home
 import androidx.compose.material.icons.outlined.Lock
@@ -13,11 +15,11 @@ import fi.project.petcare.ui.screens.WelcomeScreen
 /**
  * Contract for information needed on every PetCare navigation destination
  */
-sealed class Screen(val route: String, val icon: ImageVector? = null) {
+sealed class Screen(val route: String, val unselectedIcon: ImageVector? = null, val selectedIcon: ImageVector? = null) {
 
     object Welcome : Screen(route = "Welcome")
-    object Home : Screen(route = "Home", icon = Icons.Outlined.Home)
-    object PetProfile : Screen(route = "Profiles", icon = Icons.Outlined.Lock)
+    object Home : Screen(route = "Home", unselectedIcon = Icons.Outlined.Home, selectedIcon = Icons.Filled.Home)
+    object PetProfile : Screen(route = "Profiles", unselectedIcon = Icons.Outlined.Lock, selectedIcon = Icons.Filled.Lock)
 }
 
 // Screens to be displayed in bottom nav bar
