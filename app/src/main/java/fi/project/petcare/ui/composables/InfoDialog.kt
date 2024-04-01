1d2e27f608cf30dc893d994b004dd3f088b2c90b
Yerold <112283864+yeroldsan@users.noreply.github.com>
2024-04-01 20:01:37 +0300

    refactor: Abstract dialog to be reused

--- app/src/main/java/fi/project/petcare/ui/composables/InfoDialog.kt
+++ app/src/main/java/fi/project/petcare/ui/composables/InfoDialog.kt
@@ -1,35 +1,28 @@
 package fi.project.petcare.ui.composables
 
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Handshake
 import androidx.compose.material3.AlertDialog
-import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 
 @Composable
-fun InfoDialog(toggleDialog: () -> Unit) {
+fun InfoDialog(
+    icon: @Composable () -> Unit?,
+    title: String,
+    content: String,
+    toggleDialog: () -> Unit,
+    onConfirm: () -> Unit
+) {
     AlertDialog(
-        onDismissRequest = { /* do nothing */ },
-        icon = { Icon(Icons.Filled.Handshake, contentDescription = null) },
-        title = {
-            Text(text = "User Agreement")
-        },
-        text = {
-            Text(
-                text = "1. PetCare is for personal use only.\n" +
-                        "\n 2. We respect your privacy and won't share your data.\n" +
-                        "\n 3. You're responsible for your pet's info accuracy.\n" +
-                        "\n 4. Consult a vet for medical advice.\n" +
-                        "\n 5. Expect updates to improve PetCare.\n" +
-                        "\n Questions? Contact us at [contact@email.com].\n",
-            )
-        },
+        onDismissRequest = { toggleDialog() },
+        icon = { icon() },
+        title = { Text(text = title) },
+        text = { Text(text = content) },
         confirmButton = {
             TextButton(
                 onClick = {
                     toggleDialog()
+                    onConfirm()
                 }
             ) {
                 Text("Confirm")
