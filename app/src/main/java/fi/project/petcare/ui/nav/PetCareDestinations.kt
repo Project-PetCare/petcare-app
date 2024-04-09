590bf22463b91436e6dc39d5ecfe854279e48b1b
Yerold <112283864+yeroldsan@users.noreply.github.com>
2024-04-09 12:34:19 +0300

    feat: Add PetListScreen and update PetProfile screen

--- app/src/main/java/fi/project/petcare/ui/nav/PetCareDestinations.kt
+++ app/src/main/java/fi/project/petcare/ui/nav/PetCareDestinations.kt
@@ -1,11 +1,12 @@
 package fi.project.petcare.ui.nav
 
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Groups
 import androidx.compose.material.icons.filled.Home
-import androidx.compose.material.icons.filled.Lock
 import androidx.compose.material.icons.filled.Settings
+import androidx.compose.material.icons.outlined.Groups
 import androidx.compose.material.icons.outlined.Home
-import androidx.compose.material.icons.outlined.Lock
+import androidx.compose.material.icons.outlined.Pets
 import androidx.compose.material.icons.outlined.Settings
 import androidx.compose.ui.graphics.vector.ImageVector
 
@@ -14,16 +15,19 @@ import androidx.compose.ui.graphics.vector.ImageVector
  */
 sealed class Screen(val route: String, val unselectedIcon: ImageVector? = null, val selectedIcon: ImageVector? = null) {
 
-    object Welcome : Screen(route = "welcome")
-    object Dashboard : Screen(route = "dashboard") {
-        object Home : Screen(route = "home", unselectedIcon = Icons.Outlined.Home, selectedIcon = Icons.Filled.Home)
-        object PetProfile : Screen(route = "profile", unselectedIcon = Icons.Outlined.Lock, selectedIcon = Icons.Filled.Lock)
+    object Welcome : Screen(route = "Welcome")
+    object Dashboard : Screen(route = "Dashboard") {
+        object Home : Screen(route = "Home", unselectedIcon = Icons.Outlined.Home, selectedIcon = Icons.Filled.Home)
+        object Pets : Screen(route = "Pets", unselectedIcon = Icons.Outlined.Pets)
+        object Community : Screen(route = "Community", selectedIcon = Icons.Outlined.Groups, unselectedIcon = Icons.Filled.Groups)
     }
-    object Settings : Screen(route = "settings", unselectedIcon = Icons.Outlined.Settings, selectedIcon = Icons.Filled.Settings)
+    object PetProfile : Screen(route = "PetProfile", unselectedIcon = Icons.Outlined.Pets)
+    object Settings : Screen(route = "Settings", unselectedIcon = Icons.Outlined.Settings, selectedIcon = Icons.Filled.Settings)
 }
 
 // Screens to be displayed in bottom nav bar
 val petCareDestinations = listOf(
     Screen.Dashboard.Home,
-    Screen.Dashboard.PetProfile
+    Screen.Dashboard.Pets,
+    Screen.Dashboard.Community,
 )
