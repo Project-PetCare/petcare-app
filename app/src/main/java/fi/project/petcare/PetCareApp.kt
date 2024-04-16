8e5dc29cef52eacdd4e98a02fc00a4d6f4c9847f
Nafisa Akter <t2akna00@students.oamk.fi>
2024-04-16 14:21:43 +0600

    remove error

--- app/src/main/java/fi/project/petcare/PetCareApp.kt
+++ app/src/main/java/fi/project/petcare/PetCareApp.kt
@@ -4,15 +4,11 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.compose.rememberNavController
 import fi.project.petcare.ui.nav.NavGraph
 import fi.project.petcare.ui.theme.PetCareTheme
-import fi.project.petcare.viewmodel.AuthUiState
 import fi.project.petcare.viewmodel.AuthViewModel
 
 @Composable
@@ -33,55 +29,6 @@ fun PetCareApp() {
             modifier = Modifier.fillMaxSize(),
             color = MaterialTheme.colorScheme.background
         ) {
-<<<<<<< HEAD
-//           WelcomeScreen()
-
-
-            val navController = rememberNavController()
-            NavHost(navController = navController, startDestination = "home") {
-                // Define navigation routes
-                composable("home") {
-                    HomeScreen(navController = navController)
-                }
-                composable("profile") {
-                    ProfileScreen(petName = "Fluffy", navController = navController)
-                }
-            }
-
-
-
-        }
-    }
-}
-
-@Preview
-@Composable
-fun PetCareAppPreview() {
-    PetCareTheme (
-        darkTheme = true,
-        dynamicColor = false
-    ) {
-        Surface(
-            modifier = Modifier.fillMaxSize(),
-            color = MaterialTheme.colorScheme.background
-        ) {
-
-//           WelcomeScreen()
-
-
-            val navController = rememberNavController()
-            NavHost(navController = navController, startDestination = "home") {
-                // Define navigation routes
-                composable("home") {
-                    HomeScreen(navController = navController)
-                }
-
-
-            }
-        }
-    }
-}
-=======
             NavGraph(
                 userViewModel = authViewModel,
                 navController = navController
@@ -89,4 +36,3 @@ fun PetCareAppPreview() {
         }
     }
 }
->>>>>>> main
