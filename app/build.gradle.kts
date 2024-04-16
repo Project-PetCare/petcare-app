e5f4bf6c2b2edff02eb2405587f9bcd9f76438f0
Yerold S <112283864+yeroldsan@users.noreply.github.com>
2024-04-16 11:43:49 +0300

    chore: Remove blank lines

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -76,7 +76,6 @@ dependencies {
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
     implementation(libs.androidx.material.icons.extended)
-
     implementation(libs.androidx.lifecycle.viewmodel.compose)
     implementation(libs.androidx.navigation.compose)
     testImplementation(libs.junit)
