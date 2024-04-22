ce6bb62abd1b7411a2f63990c68c97ed44533ea9
Yerold <112283864+yeroldsan@users.noreply.github.com>
2024-04-22 13:17:44 +0300

    refactor: Simplify navigation
    Landing screen (welcome) is removed from the navigation graph. Landing
    page is shown based on the user auth state. This simplify navigation and
    shows the right screen even when the application activity has previously
    been destroyed.

--- app/src/main/java/fi/project/petcare/PetCareApp.kt
+++ app/src/main/java/fi/project/petcare/PetCareApp.kt
@@ -1,22 +1,34 @@
 package fi.project.petcare
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.compose.rememberNavController
+import fi.project.petcare.ui.composables.LoadingIndicator
 import fi.project.petcare.ui.nav.NavGraph
+import fi.project.petcare.ui.screens.WelcomeScreen
 import fi.project.petcare.ui.theme.PetCareTheme
+import fi.project.petcare.viewmodel.AuthUiState
 import fi.project.petcare.viewmodel.AuthViewModel
 
 @Composable
 fun PetCareApp() {
     val authViewModel: AuthViewModel = viewModel()
+    val authState by authViewModel.authUiState.collectAsState()
     val navController = rememberNavController()
 
-    var dynamicColors = false
+    val dynamicColors = false
     // Raw implementation of dynamic colors set by user options
 //    if (authState is AuthUiState.Authenticated) {
 //        dynamicColors = true
@@ -29,10 +41,52 @@ fun PetCareApp() {
             modifier = Modifier.fillMaxSize(),
             color = MaterialTheme.colorScheme.background
         ) {
-            NavGraph(
-                userViewModel = authViewModel,
-                navController = navController
-            )
+            when (authState) {
+                is AuthUiState.Unauthenticated -> {
+                    WelcomeScreen(vModel = authViewModel)
+                }
+                is AuthUiState.Loading -> {
+                    Scaffold { innerPadding ->
+                        Column (
+                            verticalArrangement = Arrangement.Center,
+                            horizontalAlignment = Alignment.CenterHorizontally,
+                            modifier = Modifier
+                                .padding(innerPadding)
+                                .fillMaxSize()
+                        ) {
+                            LoadingIndicator(
+                                modifier = Modifier.fillMaxSize(),
+                                color = MaterialTheme.colorScheme.primary
+                            )
+                        }
+                    }
+                }
+                is AuthUiState.Authenticated -> {
+                    // Dashboard
+                    NavGraph(
+                        navController = navController,
+                        authViewModel = authViewModel
+                    )
+                }
+                is AuthUiState.Error -> {
+                    // ErrorScreen
+                    Scaffold { innerPadding ->
+                        Column (
+                            verticalArrangement = Arrangement.Center,
+                            horizontalAlignment = Alignment.CenterHorizontally,
+                            modifier = Modifier
+                                .padding(innerPadding)
+                                .fillMaxSize()
+                        ) {
+                            Text(
+                                text = "Error: ${(authState as AuthUiState.Error).message}",
+                                style = MaterialTheme.typography.titleLarge,
+                                color = MaterialTheme.colorScheme.error
+                            )
+                        }
+                    }
+                }
+            }
         }
     }
 }
--- app/src/main/java/fi/project/petcare/ui/nav/AppNavigation.kt
+++ app/src/main/java/fi/project/petcare/ui/nav/AppNavigation.kt
@@ -2,82 +2,75 @@ package fi.project.petcare.ui.nav
 
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
-import androidx.navigation.navigation
+import fi.project.petcare.model.data.User
 import fi.project.petcare.ui.composables.Dashboard
 import fi.project.petcare.ui.screens.HomeScreen
 import fi.project.petcare.ui.screens.PetListScreen
 import fi.project.petcare.ui.screens.ProfileScreen
 import fi.project.petcare.ui.screens.SettingsScreen
-import fi.project.petcare.ui.screens.User
-import fi.project.petcare.ui.screens.WelcomeScreen
+import fi.project.petcare.viewmodel.AuthUiState
 import fi.project.petcare.viewmodel.AuthViewModel
 
 @Composable
 fun NavGraph(
     navController: NavHostController,
-    userViewModel: AuthViewModel
+    authViewModel: AuthViewModel
 ) {
+    val userState by authViewModel.authUiState.collectAsState()
+
     NavHost(
         navController = navController,
-        startDestination = Screen.Welcome.route
+        startDestination = Screen.Dashboard.Home.route
     ) {
-        composable(Screen.Welcome.route) {
-            WelcomeScreen(
-                vModel = userViewModel,
-                onUserAuthenticated = { navController.navigate(Screen.Dashboard.Home.route) }
-            )
-        }
-        navigation(
-            route = Screen.Dashboard.route,
-            startDestination = Screen.Dashboard.Home.route
-        ) {
-            composable(Screen.Dashboard.Home.route) {
-                Dashboard(
-                    onNavigateToSettings = { navController.navigate(Screen.Settings.route) },
-                    navController = navController
-                ) {
-                    HomeScreen(
-                        user = User(name = "John Doe", email = "johndoe@email.com")
-                    )
-                }
-            }
-            composable(Screen.Dashboard.Pets.route) {
-                Dashboard(
-                    onNavigateToSettings = { navController.navigate(Screen.Settings.route) },
-                    navController = navController
-                ) {
-                    PetListScreen(
-                        onNavigateToProfile = { navController.navigate(Screen.PetProfile.route) }
-                    )
-                }
-            }
-            composable(Screen.PetProfile.route) {
-                ProfileScreen(petName = "Fluffy", navController = navController)
-            }
-            composable(Screen.Dashboard.Community.route) {
-                Dashboard(
-                    onNavigateToSettings = { navController.navigate(Screen.Settings.route) },
-                    navController = navController
-                ) {
-                    Text(
-                        text = "Community content"
-                    )
-                }
-            }
-            composable(Screen.Settings.route) {
-                SettingsScreen(
-                    onNavigateBack = { navController.popBackStack() },
-                    onSignOut = {
-                        userViewModel.signOut()
-                        navController.navigate(Screen.Welcome.route) {
-                            popUpTo(Screen.Welcome.route) { inclusive = true }
-                        }
-                    }
+        composable(Screen.Dashboard.Home.route) {
+            Dashboard(
+                onNavigateToSettings = { navController.navigate(Screen.Settings.route) },
+                navController = navController
+            ) {
+                val user =
+                    if (userState is AuthUiState.Authenticated)
+                        (userState as AuthUiState.Authenticated).user
+                    else User(id = "demo-user", name = "John Doe", email = "johndoe@email.com")
+                HomeScreen(
+                    user = user
                 )
             }
+        }
+        composable(Screen.Dashboard.Pets.route) {
+            Dashboard(
+                onNavigateToSettings = { navController.navigate(Screen.Settings.route) },
+                navController = navController
+            ) {
+                PetListScreen(
+                    onNavigateToProfile = { navController.navigate(Screen.PetProfile.route) }
+                )
+            }
+        }
+        composable(Screen.PetProfile.route) {
+            ProfileScreen(petName = "Fluffy", navController = navController)
+        }
+        composable(Screen.Dashboard.Community.route) {
+            Dashboard(
+                onNavigateToSettings = { navController.navigate(Screen.Settings.route) },
+                navController = navController
+            ) {
+                Text(
+                    text = "Community content"
+                )
+            }
+        }
+        composable(Screen.Settings.route) {
+            SettingsScreen(
+                onNavigateBack = { navController.popBackStack() },
+                onSignOut = {
+                    authViewModel.onSignOut()
+                }
+            )
         }
     }
 }
