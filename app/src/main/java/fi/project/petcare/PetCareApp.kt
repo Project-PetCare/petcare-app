4d5437fe1cf158d4e1be81b6057b671788f82b8b
Yerold <112283864+yeroldsan@users.noreply.github.com>
2024-04-24 11:13:22 +0300

    feat: Show errors and loading indicators
    These are shown now on landing screen (welcome) instead of rendering
    their own screen.

--- app/src/main/java/fi/project/petcare/PetCareApp.kt
+++ app/src/main/java/fi/project/petcare/PetCareApp.kt
@@ -1,38 +1,34 @@
 package fi.project.petcare
 
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
+import android.annotation.SuppressLint
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.ui.Alignment
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.compose.rememberNavController
-import fi.project.petcare.ui.composables.LoadingIndicator
 import fi.project.petcare.ui.nav.NavGraph
 import fi.project.petcare.ui.screens.WelcomeScreen
 import fi.project.petcare.ui.theme.PetCareTheme
 import fi.project.petcare.viewmodel.AuthUiState
 import fi.project.petcare.viewmodel.AuthViewModel
+import kotlinx.coroutines.launch
 
+@SuppressLint("CoroutineCreationDuringComposition")
 @Composable
 fun PetCareApp() {
     val authViewModel: AuthViewModel = viewModel()
     val authState by authViewModel.authUiState.collectAsState()
     val navController = rememberNavController()
-
+    val scope = rememberCoroutineScope()
+    val snackbarHostState = remember { SnackbarHostState() }
     val dynamicColors = false
-    // Raw implementation of dynamic colors set by user options
-//    if (authState is AuthUiState.Authenticated) {
-//        dynamicColors = true
-//    }
 
     PetCareTheme (
         dynamicColor = dynamicColors
@@ -43,23 +39,14 @@ fun PetCareApp() {
         ) {
             when (authState) {
                 is AuthUiState.Unauthenticated -> {
-                    WelcomeScreen(vModel = authViewModel)
+                    WelcomeScreen(vModel = authViewModel, snackbarHostState = snackbarHostState)
                 }
                 is AuthUiState.Loading -> {
-                    Scaffold { innerPadding ->
-                        Column (
-                            verticalArrangement = Arrangement.Center,
-                            horizontalAlignment = Alignment.CenterHorizontally,
-                            modifier = Modifier
-                                .padding(innerPadding)
-                                .fillMaxSize()
-                        ) {
-                            LoadingIndicator(
-                                modifier = Modifier.fillMaxSize(),
-                                color = MaterialTheme.colorScheme.primary
-                            )
-                        }
-                    }
+                    WelcomeScreen(
+                        vModel = authViewModel,
+                        snackbarHostState = snackbarHostState,
+                        isLoading = true
+                    )
                 }
                 is AuthUiState.Authenticated -> {
                     // Dashboard
@@ -69,22 +56,13 @@ fun PetCareApp() {
                     )
                 }
                 is AuthUiState.Error -> {
-                    // ErrorScreen
-                    Scaffold { innerPadding ->
-                        Column (
-                            verticalArrangement = Arrangement.Center,
-                            horizontalAlignment = Alignment.CenterHorizontally,
-                            modifier = Modifier
-                                .padding(innerPadding)
-                                .fillMaxSize()
-                        ) {
-                            Text(
-                                text = "Error: ${(authState as AuthUiState.Error).message}",
-                                style = MaterialTheme.typography.titleLarge,
-                                color = MaterialTheme.colorScheme.error
-                            )
-                        }
+                    val errorMessage = (authState as AuthUiState.Error).message
+                    scope.launch {
+                        snackbarHostState.showSnackbar(
+                            message = errorMessage,
+                        )
                     }
+                    WelcomeScreen(vModel = authViewModel, snackbarHostState = snackbarHostState)
                 }
             }
         }
