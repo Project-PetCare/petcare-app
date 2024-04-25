77b8854814be7b0500eb1cd248265fb2f8185e39
Yerold <112283864+yeroldsan@users.noreply.github.com>
2024-04-25 17:56:57 +0300

    feat: Use supabase instance
    Pass supabase instance client and check for authenticated users to get
    user id.

--- app/src/main/java/fi/project/petcare/viewmodel/PetViewModel.kt
+++ app/src/main/java/fi/project/petcare/viewmodel/PetViewModel.kt
@@ -3,11 +3,11 @@ package fi.project.petcare.viewmodel
 import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import fi.project.petcare.BuildConfig
 import fi.project.petcare.model.data.PetResponse
+import fi.project.petcare.model.data.SupabaseClientFactory
 import fi.project.petcare.model.repository.PetRepository
-import io.github.jan.supabase.createSupabaseClient
-import io.github.jan.supabase.postgrest.Postgrest
+import io.github.jan.supabase.gotrue.SessionStatus
+import io.github.jan.supabase.gotrue.auth
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
@@ -19,11 +19,7 @@ sealed interface PetUiState {
 }
 
 class PetViewModel: ViewModel() {
-    private val apiUrl = BuildConfig.SUPABASE_URL
-    private val apiKey = BuildConfig.SUPABASE_KEY
-    private val client = createSupabaseClient(apiUrl, apiKey) {
-        install(Postgrest)
-    }
+    private val client = SupabaseClientFactory.getInstance()
 
     private val _petUiState = MutableStateFlow<PetUiState>(PetUiState.Loading)
     val petUiState: StateFlow<PetUiState> = _petUiState
@@ -34,15 +30,36 @@ class PetViewModel: ViewModel() {
 
     private fun getPet() {
         viewModelScope.launch {
-            val result = PetRepository(client).getPets()
-            Log.i("PetViewModel", "getPet: $result")
+            client.auth.sessionStatus.collect {
+                when (it) {
+                    is SessionStatus.Authenticated -> {
+                        val result = PetRepository(client).getPets(it.session.user?.id.toString())
+                        result.fold(
+                            onSuccess = { pets -> _petUiState.value = PetUiState.Success(pets) },
+                            onFailure = { e ->
+                                Log.e("PetViewModel", "User not authenticated", e)
+                                _petUiState.value = PetUiState.Error(e.message ?: "Unknown error")
+                            }
+                        )
+                    }
+                    else -> {
+                        _petUiState.value = PetUiState.Error("User not authenticated")
+                    }
+                }
+            }
+        }
+    }
+
+    fun upsertPet(pet: PetResponse.Pet) {
+        viewModelScope.launch {
+            val result = PetRepository(client).upsertPet(pet)
             result.fold(
-                onSuccess = { pets -> _petUiState.value = PetUiState.Success(pets) },
+                onSuccess = { getPet() },
                 onFailure = { e ->
+                    Log.e("PetViewModel", "Error upserting pet:", e)
                     _petUiState.value = PetUiState.Error(e.message ?: "Unknown error")
                 }
             )
         }
     }
-
 }
