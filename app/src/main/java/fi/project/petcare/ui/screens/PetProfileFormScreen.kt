1805e6022bc96fd40cd219dec0e8bc6e440f3a30
Nafisa Akter <t2akna00@students.oamk.fi>
2024-04-27 00:11:57 +0600

    updated some design

--- app/src/main/java/fi/project/petcare/ui/screens/PetProfileFormScreen.kt
+++ app/src/main/java/fi/project/petcare/ui/screens/PetProfileFormScreen.kt
@@ -1,16 +1,11 @@
 package fi.project.petcare.ui.screens
 
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Animation
 import androidx.compose.material.icons.filled.CalendarViewMonth
@@ -19,8 +14,8 @@ import androidx.compose.material.icons.filled.MonitorWeight
 import androidx.compose.material.icons.filled.Numbers
 import androidx.compose.material.icons.filled.Pets
 import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -30,14 +25,11 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import fi.project.petcare.ui.theme.md_theme_dark_onPrimary
-import fi.project.petcare.ui.theme.md_theme_light_primary
 
 @Composable
 fun PetProfileFormScreen() {
@@ -60,16 +52,15 @@ fun PetProfileFormScreen() {
     Column(
         modifier = Modifier
             .fillMaxHeight()
-            .padding(16.dp)
-            .background(Color.Transparent),
+            .padding(16.dp),
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
         Text(
             text = "Create a account for you pet",
-            fontWeight = FontWeight.Bold,
-            fontSize = 20.sp,
+            style = MaterialTheme.typography.headlineMedium,
+            modifier = Modifier.padding(bottom = 8.dp)
 
-            modifier = Modifier.padding(bottom = 16.dp)
+
         )
 
         // Microchip ID field with validation
@@ -79,18 +70,23 @@ fun PetProfileFormScreen() {
                 microchipId = it
                 microchipIdError = if (it.isEmpty()) "Microchip ID cannot be empty" else ""
             },
+            keyboardOptions = KeyboardOptions(
+                keyboardType = KeyboardType.Text,
+                imeAction = ImeAction.Next,
+            ),
+            shape = MaterialTheme.shapes.large,
             label = { Text("Microchip ID") },
             leadingIcon = { Icon(imageVector = Icons.Default.Numbers, contentDescription = null) },
             isError = microchipIdError.isNotEmpty(),
             singleLine = true,
-            modifier = Modifier.padding(bottom = 8.dp)
+            modifier = Modifier.padding(bottom = 16.dp)
         )
         Text(
             text = microchipIdError,
             color = Color.Red,
-            fontSize = 12.sp,
-            modifier = Modifier.padding(start = 16.dp)
-        )
+
+
+            )
 
         // Pet name field with validation
         OutlinedTextField(
@@ -99,36 +95,46 @@ fun PetProfileFormScreen() {
                 name = it
                 petNameError = if (it.isEmpty()) "Pet name cannot be empty" else ""
             },
+            keyboardOptions = KeyboardOptions(
+                keyboardType = KeyboardType.Text,
+                imeAction = ImeAction.Next,
+            ),
+            shape = MaterialTheme.shapes.large,
             label = { Text("Pet Name") },
             leadingIcon = { Icon(imageVector = Icons.Default.Pets, contentDescription = null) },
             isError = petNameError.isNotEmpty(),
             singleLine = true,
-            modifier = Modifier.padding(bottom = 8.dp)
+
+            modifier = Modifier.padding(bottom = 16.dp)
         )
         Text(
             text = petNameError,
             color = Color.Red,
-            fontSize = 12.sp,
-            modifier = Modifier.padding(start = 16.dp)
-        )
+
+
+            )
 
         // Gender field with validation
 
         OutlinedTextField(
             value = selectedGender,
             onValueChange = { selectedGender = it },
+            keyboardOptions = KeyboardOptions(
+                keyboardType = KeyboardType.Text,
+                imeAction = ImeAction.Next,
+            ),
+            shape = MaterialTheme.shapes.large,
             label = { Text("Gender") },
             leadingIcon = { Icon(imageVector = Icons.Default.Category, contentDescription = null) },
             isError = genderError.isNotEmpty(),
             singleLine = true,
-            modifier = Modifier.padding(bottom = 8.dp)
+            modifier = Modifier.padding(bottom = 16.dp)
         )
         Text(
             text = genderError,
             color = Color.Red,
-            fontSize = 12.sp,
-            modifier = Modifier.padding(start = 16.dp)
-        )
+
+            )
         // Weight field with validation
         OutlinedTextField(
             value = weight,
@@ -136,18 +142,22 @@ fun PetProfileFormScreen() {
                 weight = it
                 weightError = if (it.isEmpty()) "Weight cannot be empty" else ""
             },
+            keyboardOptions = KeyboardOptions(
+                keyboardType = KeyboardType.Text,
+                imeAction = ImeAction.Next,
+            ),
+            shape = MaterialTheme.shapes.large,
             label = { Text("Weight") },
             leadingIcon = { Icon(imageVector = Icons.Default.MonitorWeight, contentDescription = null) },
             isError = weightError.isNotEmpty(),
             singleLine = true,
-            modifier = Modifier.padding(bottom = 8.dp)
+            modifier = Modifier.padding(bottom = 16.dp)
         )
         Text(
             text = weightError,
             color = Color.Red,
-            fontSize = 12.sp,
-            modifier = Modifier.padding(start = 16.dp)
-        )
+
+            )
 
         // Species field with validation
         OutlinedTextField(
@@ -156,18 +166,22 @@ fun PetProfileFormScreen() {
                 species = it
                 speciesError = if (it.isEmpty()) "Species cannot be empty" else ""
             },
+            keyboardOptions = KeyboardOptions(
+                keyboardType = KeyboardType.Text,
+                imeAction = ImeAction.Next,
+            ),
+            shape = MaterialTheme.shapes.large,
             label = { Text("Species") },
             leadingIcon = { Icon(imageVector = Icons.Default.Animation, contentDescription = null) },
             isError = speciesError.isNotEmpty(),
             singleLine = true,
-            modifier = Modifier.padding(bottom = 8.dp)
+            modifier = Modifier.padding(bottom = 16.dp)
         )
         Text(
             text = speciesError,
             color = Color.Red,
-            fontSize = 12.sp,
-            modifier = Modifier.padding(start = 16.dp)
-        )
+
+            )
 
         // Breed field with validation
         OutlinedTextField(
@@ -177,17 +191,21 @@ fun PetProfileFormScreen() {
                 breedError = if (it.isEmpty()) "Breed cannot be empty" else ""
             },
             label = { Text("Breed") },
+            keyboardOptions = KeyboardOptions(
+                keyboardType = KeyboardType.Text,
+                imeAction = ImeAction.Next,
+            ),
+            shape = MaterialTheme.shapes.large,
             leadingIcon = { Icon(imageVector = Icons.Default.Pets, contentDescription = null) },
             isError = breedError.isNotEmpty(),
             singleLine = true,
-            modifier = Modifier.padding(bottom = 8.dp)
+            modifier = Modifier.padding(bottom = 16.dp)
         )
         Text(
             text = breedError,
             color = Color.Red,
-            fontSize = 12.sp,
-            modifier = Modifier.padding(start = 16.dp)
-        )
+
+            )
 
         // Birthdate field with validation
         OutlinedTextField(
@@ -196,54 +214,38 @@ fun PetProfileFormScreen() {
                 age_months = it
                 age_monthsError = if (it.isEmpty()) "Age cannot be empty" else ""
             },
+            keyboardOptions = KeyboardOptions(
+                keyboardType = KeyboardType.Text,
+                imeAction = ImeAction.Next,
+            ),
+            shape = MaterialTheme.shapes.large,
             label = { Text("Age") },
             leadingIcon = { Icon(imageVector = Icons.Default.CalendarViewMonth, contentDescription = null) },
             isError = age_monthsError.isNotEmpty(),
             singleLine = true,
-            modifier = Modifier.padding(bottom = 8.dp)
+            modifier = Modifier.padding(bottom = 16.dp)
         )
         Text(
             text = age_monthsError,
             color = Color.Red,
-            fontSize = 12.sp,
-            modifier = Modifier.padding(start = 16.dp)
-        )
 
-        Spacer(modifier = Modifier.height(16.dp))
+            )
+
 
         // Save button
         Button(
-            onClick = { /* Save the form data if validation passes */ },
+            onClick = {
+
+            },
+
             modifier = Modifier
                 .fillMaxWidth()
-                .heightIn(48.dp),
-            contentPadding = PaddingValues(),
-            colors = ButtonDefaults.buttonColors(Color.Transparent)
+                .padding(horizontal = 64.dp)
+                .height(58.dp)
         ) {
-            Box(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .heightIn(48.dp)
-                    .background(
-                        brush = Brush.horizontalGradient(
-                            listOf(
-                                md_theme_dark_onPrimary,
-                                md_theme_light_primary
-                            )
-                        ),
-                        shape = RoundedCornerShape(50.dp)
-                    ),
-                contentAlignment = Alignment.Center
-            ) {
-                Text(
-                    "Submit",
-                    fontSize = 18.sp,
-                    fontWeight = FontWeight.Bold
-                )
-            }
+            Text("Submit")
         }
-    }
-}
+    }}
 
 @Preview
 @Composable
