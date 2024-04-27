207338f4e45f4f21d5ccd92d9669218d921a5bce
Yerold <112283864+yeroldsan@users.noreply.github.com>
2024-04-27 21:06:57 +0300

    feat: Make pets variable optional
    An empty list of pets could be returned from the back-end. UI should
    should be successful but instead of a empty list we show a message to
    the user.

--- app/src/main/java/fi/project/petcare/model/data/Pet.kt
+++ app/src/main/java/fi/project/petcare/model/data/Pet.kt
@@ -4,7 +4,7 @@ import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
 data class PetResponse(
-    val pets: List<Pet>
+    val pets: List<Pet>? = null,
 ) {
     @Serializable
     data class Pet(
