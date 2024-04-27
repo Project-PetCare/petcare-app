a30dab3c6f53a5c15e64fad5e957842552ad646d
Yerold <112283864+yeroldsan@users.noreply.github.com>
2024-04-27 19:08:00 +0300

    refactor: Use pet object passed to screen
    Instead of using separate variables for input field states, a Pet object
    is now passed for default values. onChange updates a single object
    instead of separate fields.

--- app/src/main/java/fi/project/petcare/ui/screens/PetProfileFormScreen.kt
+++ app/src/main/java/fi/project/petcare/ui/screens/PetProfileFormScreen.kt
@@ -1,9 +1,9 @@
 package fi.project.petcare.ui.screens
 
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Animation
@@ -18,8 +18,6 @@ import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableDoubleStateOf
-import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
@@ -30,25 +28,23 @@ import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import fi.project.petcare.model.data.PetImpl
 
 @Preview
 @Composable
 fun PetProfileFormScreenPreview() {
-    PetProfileFormScreen(innerPaddingValues = PaddingValues(8.dp))
+//    PetProfileFormScreen(
+//        innerPaddingValues = PaddingValues(8.dp),
+//
+//    )
 }
 
 @Composable
 fun PetProfileFormScreen(
     innerPaddingValues: PaddingValues,
+    newPet: PetImpl,
+    onUpdatePet: (PetImpl) -> Unit
 ) {
-    // State variables to hold form values and validation errors
-    var microchipId by remember { mutableIntStateOf(0) }
-    var name by remember { mutableStateOf("") }
-    var weight by remember { mutableDoubleStateOf(0.0) }
-    var species by remember { mutableStateOf("") }
-    var breed by remember { mutableStateOf("") }
-    var ageMonths by remember { mutableIntStateOf(0) }
-    var selectedGender by remember { mutableStateOf("") }
     var petNameError by remember { mutableStateOf("") }
     var genderError by remember { mutableStateOf("") }
     var weightError by remember { mutableStateOf("") }
@@ -57,167 +53,177 @@ fun PetProfileFormScreen(
     var ageMonthsError by remember { mutableStateOf("") }
     var microchipIdError by remember { mutableStateOf("") }
 
-    Column(
+    LazyColumn(
         modifier = Modifier
             .padding(innerPaddingValues)
             .padding(16.dp)
             .fillMaxSize(),
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        // Microchip ID field with validation
-        OutlinedTextField(
-            value = microchipId.toString(), // Convert Int to String for TextField
-            onValueChange = {
-                microchipId = it.toIntOrNull() ?: 0 // Convert String to Int or default to 0 if conversion fails
-                microchipIdError = if (it.isEmpty()) "Microchip ID cannot be empty" else ""
-            },
-            keyboardOptions = KeyboardOptions(
-                keyboardType = KeyboardType.Number, // Numeric keyboard type
-                imeAction = ImeAction.Next,
-            ),
-            shape = MaterialTheme.shapes.large,
-            label = {
-                Text(
-                    "Microchip ID",
-                )
-            },
-            leadingIcon = { Icon(imageVector = Icons.Default.Numbers, contentDescription = null) },
-            isError = microchipIdError.isNotEmpty(),
-            singleLine = true,
-        )
-        Text(
-            text = microchipIdError,
-            color = Color.Red
-        )
+        item {
+            // Microchip ID field with validation
+            OutlinedTextField(
+                value = newPet.microchipId.toString(), // Convert Int to String for TextField
+                onValueChange = {
+                    onUpdatePet( newPet.copy(microchipId = it.toIntOrNull() ?: 0) )
+                    microchipIdError = if (it.isEmpty()) "Microchip ID cannot be empty" else ""
+                },
+                keyboardOptions = KeyboardOptions(
+                    keyboardType = KeyboardType.Number, // Numeric keyboard type
+                    imeAction = ImeAction.Next,
+                ),
+                shape = MaterialTheme.shapes.large,
+                label = {
+                    Text(
+                        text = "Microchip ID",
+                    )
+                },
+                leadingIcon = { Icon(imageVector = Icons.Default.Numbers, contentDescription = null) },
+                isError = microchipIdError.isNotEmpty(),
+                singleLine = true,
+            )
+            Text(
+                text = microchipIdError,
+                color = Color.Red
+            )
+        }
+        item {
+            // Pet name field with validation
+            OutlinedTextField(
+                value = newPet.name,
+                onValueChange = {
+                    onUpdatePet(newPet.copy(name = it))
+                    petNameError = if (it.isEmpty()) "Pet name cannot be empty" else ""
+                },
+                keyboardOptions = KeyboardOptions(
+                    keyboardType = KeyboardType.Text,
+                    imeAction = ImeAction.Next,
+                ),
+                shape = MaterialTheme.shapes.large,
+                label = { Text("Pet Name") },
+                leadingIcon = { Icon(imageVector = Icons.Default.Pets, contentDescription = null) },
+                isError = petNameError.isNotEmpty(),
+                singleLine = true
+            )
+            Text(
+                text = petNameError,
+                color = Color.Red,
+            )
+        }
+        item {
+            // Gender field with validation
+            OutlinedTextField(
+                value = newPet.gender,
+                onValueChange = {
+                    onUpdatePet(newPet.copy(gender = it))
+                },
+                keyboardOptions = KeyboardOptions(
+                    keyboardType = KeyboardType.Text,
+                    imeAction = ImeAction.Next,
+                ),
+                shape = MaterialTheme.shapes.large,
+                label = { Text("Gender") },
+                leadingIcon = { Icon(imageVector = Icons.Default.Category, contentDescription = null) },
+                isError = genderError.isNotEmpty(),
+                singleLine = true
+            )
+            Text(
+                text = genderError,
+                color = Color.Red,
+            )
+        }
+        item {
+            // Weight field with validation
+            OutlinedTextField(
+                value = newPet.weight.toString(),
+                onValueChange = {
+                    onUpdatePet(newPet.copy(weight = it.toDoubleOrNull() ?: 0.0))
+                    weightError = if (it.isEmpty()) "Weight cannot be empty" else ""
+                },
+                keyboardOptions = KeyboardOptions(
+                    keyboardType = KeyboardType.Number,
+                    imeAction = ImeAction.Next,
+                ),
+                shape = MaterialTheme.shapes.large,
+                label = { Text("Weight") },
+                leadingIcon = { Icon(imageVector = Icons.Default.MonitorWeight, contentDescription = null) },
+                isError = weightError.isNotEmpty(),
+                singleLine = true
+            )
+            Text(
+                text = weightError,
+                color = Color.Red,
+            )
+        }
+        item {
+            // Species field with validation
+            OutlinedTextField(
+                value = newPet.species,
+                onValueChange = {
+                    onUpdatePet(newPet.copy(species = it))
+                    speciesError = if (it.isEmpty()) "Species cannot be empty" else ""
+                },
+                keyboardOptions = KeyboardOptions(
+                    keyboardType = KeyboardType.Text,
+                    imeAction = ImeAction.Next,
+                ),
+                shape = MaterialTheme.shapes.large,
+                label = { Text("Species") },
+                leadingIcon = { Icon(imageVector = Icons.Default.Animation, contentDescription = null) },
+                isError = speciesError.isNotEmpty(),
+                singleLine = true
+            )
+            Text(
+                text = speciesError,
+                color = Color.Red,
+            )
+        }
+        item {
+            // Breed field with validation
+            OutlinedTextField(
+                value = newPet.breed,
+                onValueChange = {
+                    onUpdatePet(newPet.copy(breed = it))
+                    breedError = if (it.isEmpty()) "Breed cannot be empty" else ""
+                },
+                label = { Text("Breed") },
+                keyboardOptions = KeyboardOptions(
+                    keyboardType = KeyboardType.Text,
+                    imeAction = ImeAction.Next,
+                ),
+                shape = MaterialTheme.shapes.large,
+                leadingIcon = { Icon(imageVector = Icons.Default.Pets, contentDescription = null) },
+                isError = breedError.isNotEmpty(),
+                singleLine = true
+            )
+            Text(
+                text = breedError,
+                color = Color.Red,
+            )
+        }
+        item {
+            // Birthdate field with validation
+            OutlinedTextField(
+                value = newPet.ageMonths.toString(), // Convert Double to String for TextField
+                onValueChange = {
+                    onUpdatePet(newPet.copy(ageMonths = it.toIntOrNull() ?: 0))
+                    ageMonthsError = if (it.isEmpty()) "Age cannot be empty" else ""
+                },
+                keyboardOptions = KeyboardOptions(
+                    keyboardType = KeyboardType.Number,
+                    imeAction = ImeAction.Done,
+                ),
+                shape = MaterialTheme.shapes.large,
+                label = { Text("Age") },
+                leadingIcon = { Icon(imageVector = Icons.Default.CalendarViewMonth, contentDescription = null) },
+                isError = ageMonthsError.isNotEmpty(),
+                singleLine = true
+            )
 
-        // Pet name field with validation
-        OutlinedTextField(
-            value = name,
-            onValueChange = {
-                name = it
-                petNameError = if (it.isEmpty()) "Pet name cannot be empty" else ""
-            },
-            keyboardOptions = KeyboardOptions(
-                keyboardType = KeyboardType.Text,
-                imeAction = ImeAction.Next,
-            ),
-            shape = MaterialTheme.shapes.large,
-            label = { Text("Pet Name") },
-            leadingIcon = { Icon(imageVector = Icons.Default.Pets, contentDescription = null) },
-            isError = petNameError.isNotEmpty(),
-            singleLine = true
-        )
-        Text(
-            text = petNameError,
-            color = Color.Red,
-        )
-
-        // Gender field with validation
-
-        OutlinedTextField(
-            value = selectedGender,
-            onValueChange = { selectedGender = it },
-            keyboardOptions = KeyboardOptions(
-                keyboardType = KeyboardType.Text,
-                imeAction = ImeAction.Next,
-            ),
-            shape = MaterialTheme.shapes.large,
-            label = { Text("Gender") },
-            leadingIcon = { Icon(imageVector = Icons.Default.Category, contentDescription = null) },
-            isError = genderError.isNotEmpty(),
-            singleLine = true
-        )
-        Text(
-            text = genderError,
-            color = Color.Red,
-        )
-        // Weight field with validation
-        OutlinedTextField(
-            value = weight.toString(),
-            onValueChange = {
-                weight = it.toDoubleOrNull() ?: 0.0
-                weightError = if (it.isEmpty()) "Weight cannot be empty" else ""
-            },
-            keyboardOptions = KeyboardOptions(
-                keyboardType = KeyboardType.Text,
-                imeAction = ImeAction.Next,
-            ),
-            shape = MaterialTheme.shapes.large,
-            label = { Text("Weight") },
-            leadingIcon = { Icon(imageVector = Icons.Default.MonitorWeight, contentDescription = null) },
-            isError = weightError.isNotEmpty(),
-            singleLine = true
-        )
-        Text(
-            text = weightError,
-            color = Color.Red,
-        )
-
-        // Species field with validation
-        OutlinedTextField(
-            value = species,
-            onValueChange = {
-                species = it
-                speciesError = if (it.isEmpty()) "Species cannot be empty" else ""
-            },
-            keyboardOptions = KeyboardOptions(
-                keyboardType = KeyboardType.Text,
-                imeAction = ImeAction.Next,
-            ),
-            shape = MaterialTheme.shapes.large,
-            label = { Text("Species") },
-            leadingIcon = { Icon(imageVector = Icons.Default.Animation, contentDescription = null) },
-            isError = speciesError.isNotEmpty(),
-            singleLine = true
-        )
-        Text(
-            text = speciesError,
-            color = Color.Red,
-        )
-
-        // Breed field with validation
-        OutlinedTextField(
-            value = breed,
-            onValueChange = {
-                breed = it
-                breedError = if (it.isEmpty()) "Breed cannot be empty" else ""
-            },
-            label = { Text("Breed") },
-            keyboardOptions = KeyboardOptions(
-                keyboardType = KeyboardType.Text,
-                imeAction = ImeAction.Next,
-            ),
-            shape = MaterialTheme.shapes.large,
-            leadingIcon = { Icon(imageVector = Icons.Default.Pets, contentDescription = null) },
-            isError = breedError.isNotEmpty(),
-            singleLine = true
-        )
-        Text(
-            text = breedError,
-            color = Color.Red,
-        )
-
-        // Birthdate field with validation
-        OutlinedTextField(
-            value = ageMonths.toString(), // Convert Double to String for TextField
-            onValueChange = {
-                ageMonths = it.toIntOrNull() ?: 0  // Convert String to Double or default to 0.0 if conversion fails
-                ageMonthsError = if (it.isEmpty()) "Age cannot be empty" else ""
-            },
-            keyboardOptions = KeyboardOptions(
-                keyboardType = KeyboardType.Number,
-                imeAction = ImeAction.Next,
-            ),
-            shape = MaterialTheme.shapes.large,
-            label = { Text("Age") },
-            leadingIcon = { Icon(imageVector = Icons.Default.CalendarViewMonth, contentDescription = null) },
-            isError = ageMonthsError.isNotEmpty(),
-            singleLine = true
-        )
-
-        Text(
-            text = ageMonthsError,
-            color = Color.Red
-        )
+            Text(
+                text = ageMonthsError,
+                color = Color.Red
+            )
+        }
     }
 }
